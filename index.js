3b6f7e1a3d083a8a3b62d318878ee87362325de4
unknown <apurbahasanj@gmail.com>
2023-05-03 02:10:26 +0600

    chef data added to the server

--- index.js
+++ index.js
@@ -1,23 +1,27 @@
-const express = require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
+const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 // Enable CORS middleware
 app.use(cors());
-  
 
-const bannerFood = require('./data/BannerFood.json')
+const bannerFood = require("./data/BannerFood.json");
+const chef = require("./data/Chef.json");
 
-app.get('/', (req, res) => {
-    res.send('Welcome')
-})
+app.get("/", (req, res) => {
+  res.send("Welcome");
+});
 // https://seoul-kitchenista-server-apurbahasanj.vercel.app/
 
-app.get('/banner-food', (req, res) => {
-    res.send(bannerFood)
+app.get("/banner-food", (req, res) => {
+  res.send(bannerFood);
+});
+
+app.get('/chef', (req, res) => {
+    res.send(chef);
 })
 
-app.listen(port, ()=>{
-    console.log(`Example app listening on port ${port}`);
-})
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
