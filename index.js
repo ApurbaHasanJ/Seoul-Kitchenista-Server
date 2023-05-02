d10ee9aee8164789ca9e2e1781ad65ab940f7f7e
unknown <apurbahasanj@gmail.com>
2023-05-02 22:16:10 +0600

    bannder data server ready

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express');
 const app = express();
 const port = process.env.PORT || 5000;
 
+const bannerFood = require('./data/BannerFood.json')
+
 app.get('/', (req, res) => {
     res.send('Welcome')
+})
+// https://seoul-kitchenista-server-apurbahasanj.vercel.app/
+
+app.get('/banner-food', (req, res) => {
+    res.send(bannerFood)
 })
 
 app.listen(port, ()=>{
